b7437fa1d620316faa6091238542fa2b00e3e2f3
DeveloperWide <maheshisdelta@gmail.com>
2026-01-12 23:29:30 +0530

    first Commit

---
+++ Day_1/JavaBasics.java
@@ -0,0 +1,9 @@
+public class JavaBasics {
+    public static void main(String args[]){
+        // Print a Pattern
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
